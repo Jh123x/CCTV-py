95409960091109a64c06dff96b8d63dd16fa2362
Junhua <wenjh1998@gmail.com>
2020-11-01 18:25:16 +0800

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,10 +1,13 @@
 import socket
 import pickle
+import cv2
+import camera
 
 class CCTVClient:
     def __init__(self, ip:str, port:int):
         """Create a client object to send the frames"""
         self.addr = (ip, port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def get_addr(self) -> tuple:
         """Get the (ip,port) of the server"""
@@ -12,7 +15,7 @@ class CCTVClient:
 
     def connect(self):
         """Connect to the server"""
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(self.get_addr())
 
     def send(self, obj) -> None:
         """Send an object to the server"""
@@ -20,3 +23,27 @@ class CCTVClient:
 
     def close(self):
         self.socket.close()
+
+def main():
+    """Main function of the client"""
+
+    #Create a CCTV client
+    client = CCTVClient("localhost", 1234)
+
+    #Initialise the queue
+    queue = camera.initialise()
+
+    #Start polling
+    camera.start_poll(0, queue)
+
+    client.connect()
+    while True:
+        image = queue.get()
+        retval, buffer = cv2.imencode('.jpg', image)
+        client.send(buffer)
+
+    client.close()
+        
+
+if __name__ == "__main__":
+    main()
